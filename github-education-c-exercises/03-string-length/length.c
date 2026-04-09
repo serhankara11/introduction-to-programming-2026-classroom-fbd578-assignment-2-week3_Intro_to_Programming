c0a57b14c61c401a84bc78619f5cab962f21bcac
serhankara11 <kara.serhan@ibu.edu.mk>
2026-04-09 22:18:58 +0200

    Update length.c

--- github-education-c-exercises/03-string-length/length.c
+++ github-education-c-exercises/03-string-length/length.c
@@ -1,17 +1,32 @@
+
 #include <cs50.h>
 #include <stdio.h>
 
+// Function prototype to calculate string length
 int string_length(string s);
 
 int main(void)
 {
+    // Get a name from the user
     string name = get_string("Name: ");
+    
+    // Calculate the length using our custom function
     int len = string_length(name);
+    
+    // Print the result
     printf("%i\n", len);
 }
 
+// Custom function to count characters until the NUL terminator '\0'
 int string_length(string s)
 {
-    // TODO: count chars until '\0'
-    return 0;
+    int n = 0;
+    
+    // Keep counting as long as the current character is not '\0'
+    while (s[n] != '\0')
+    {
+        n++;
+    }
+    
+    return n;
 }
