5a4a84d1e0b647fde48e58b035decee284a8acc2
serhankara11 <kara.serhan@ibu.edu.mk>
2026-04-09 22:20:25 +0200

    Update greet.c

--- github-education-c-exercises/05-argv-greet/greet.c
+++ github-education-c-exercises/05-argv-greet/greet.c
@@ -3,8 +3,17 @@
 
 int main(int argc, string argv[])
 {
-    // TODO:
-    // if argc == 2 print hello, argv[1]
-    // else print hello, world
+    // Check if the user provided exactly one argument (their name)
+    if (argc == 2)
+    {
+        // argv[0] is the program name, argv[1] is the first word provided
+        printf("hello, %s\n", argv[1]);
+    }
+    else
+    {
+        // Default message if no name is provided
+        printf("hello, world\n");
+    }
+    
     return 0;
 }
