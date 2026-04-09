3de08e557afb4e8793ce63973a629527fbd5a2c0
serhankara11 <kara.serhan@ibu.edu.mk>
2026-04-09 22:14:11 +0200

    Update buggy2.c

--- github-education-c-exercises/01-debugging/buggy2.c
+++ github-education-c-exercises/01-debugging/buggy2.c
@@ -1,8 +1,8 @@
-// Missing cs50.h, variable's type, semicolon, %s, and second printf argument.
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    name = get_string("What's your name? ")
-    printf("hello, world\n");
+    string name = get_string("What's your name? ");
+    printf("hello, %s\n", name);
 }
